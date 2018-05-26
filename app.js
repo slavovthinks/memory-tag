6f0f201322c9d913e0192dde6a64b8ab77d4ef7c
Chavdar Dimitrov <chavdar@fidor.com>
2018-05-26 18:12:25 +0300

    Finishing upload image logic.

--- app.js
+++ app.js
@@ -5,12 +5,12 @@ const upload = require('jquery-file-upload-middleware');
 const bodyParser = require('body-parser')
 const app = express()
 const nunjucks = require('nunjucks')
+const shortID = require('shortid');
 
 // const testRoute = require('./routes/test')
 const uploadRoute = require('./routes/uploadRoute')
 const homeRoute = require('./routes/homeRoute');
 // var souvenirs = require('./routes/souvenirs');
-
 
 nunjucks.configure('templates', {
   autoescape: true,
@@ -29,20 +29,13 @@ upload.configure({
     }
 });
 
-// app.configure(function () {
-
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }))
 
 // parse application/json
 app.use(bodyParser.json())
 
-    // app.use('/upload_media', upload.fileHandler());
-    // app.use(express.bodyParser());
-
-// });
-
-/// Redirect all to home except post
+// Redirect all to home except post
 // app.get('/upload_media', function( req, res ){
 //     res.redirect('/');
 // });
@@ -55,40 +48,29 @@ app.use(bodyParser.json())
 //     res.redirect('/');
 // });
 
-upload.on('begin', function (fileInfo, req, res) {
+upload.on('begin', function (fileInfo) {
+    var ext = fileInfo.name.substr(fileInfo.name.lastIndexOf('.') + 1);
+    var shortid = shortID;
+    fileInfo.name = shortid.generate() + '.' + ext;
+});
+
+upload.on('end', function (fileInfo, req, res) {
     console.log(fileInfo);
-    // fileInfo structure is the same as returned to browser
-    // {
-    //     name: '3 (3).jpg',
-    //     originalName: '3.jpg',
-    //     size: 79262,
-    //     type: 'image/jpeg',
-    //     delete_type: 'DELETE',
-    //     delete_url: 'http://yourhost/upload/3%20(3).jpg',
-    //     url: 'http://yourhost/uploads/3%20(3).jpg',
-    //     thumbnail_url: 'http://youhost/uploads/thumbnail/3%20(3).jpg'
-    // }
 });
 
 app.use('/upload_media', function(req, res, next){
-    console.log(req);
-    console.log('helllloooow');
     upload.fileHandler({
         uploadDir: function () {
-            return __dirname + '/public/uploads/' + req.sessionId
+            return __dirname + '/public/uploads/'
         },
         uploadUrl: function () {
-            return '/upload/' + req.sessionId
+            return '/uploads'
         }
     })(req, res, next);
 });
 
-
-
-
 // Enable CORS
 app.use(cors())
-
 
 // JS CSS AND ASSETS
 app.use(express.static('static'))
