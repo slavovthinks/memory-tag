ccec8ea92b18a4a8b2ccec518c365b519b61a9aa
d-enchew <danail.enchew@gmail.com>
2018-05-26 10:18:09 +0300

    Bullshit!

--- routes/test.js
+++ routes/test.js
@@ -1,8 +1,17 @@
 const { Router } = require('express')
 const router = Router()
-
+const { TestModel } = require('../models')
 router.get('/', (req, res) => {
-  res.render('test.html', {test: 'This is a test!'})
+  res.render('test.html', {test: 'This is a test!', a: 'This is A'})
 })
+router.get('/souvenirs/:id', (req, res) => {
+  let souvenir = souvenirsModel.getOne(id)
 
+  if (souvenir.registered) {
+  	// Play video
+  } else {
+  	// build video
+  }
+  res.render('test.html', {test: 'blqqq!'})
+})
 module.exports = router
