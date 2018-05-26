4251fad5809a2af191bedd4232b1958f4ca3f7b5
d-enchew <danail.enchew@gmail.com>
2018-05-26 13:08:03 +0300

    upload page

--- app.js
+++ app.js
@@ -3,13 +3,14 @@ const config = require('config')
 const app = express()
 const nunjucks = require('nunjucks')
 
+const testRoute = require('./routes/test')
+const uploadRoute = require('./routes/upload')
+
 nunjucks.configure('templates', {
   autoescape: true,
   express: app
 })
-
-const testRoute = require('./routes/test')
-const uploadRoute = require('./routes/upload')
+app.use(express.static('static'))
 
 app.use('/souvenirs', testRoute)
 app.get('/upload', uploadRoute)
